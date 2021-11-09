0389b15013c5903c5638c991559e8e42ec6e2ada
Ian Gagorik <ian.gagorik@gmail.com>
2021-11-08 16:05:55 -0800

    Changing nickles

--- src/Coinchanger.sol
+++ src/Coinchanger.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.8.0;
 
 struct Change {
     uint256 pennies;
+    uint256 nickles;
 }
 
 contract Coinchanger {
     function makeChange(uint256 _coins) public pure returns (Change memory) {
-        return Change(_coins);
+        Change memory change = Change(0, 0);
+        uint256 coins = _coins;
+
+        if (coins >= 5) {
+            coins = coins - 5;
+            change.nickles = 1;
+        }
+        change.pennies = coins;
+
+        return change;
     }
 }
--- src/test/Coinchanger.t.sol
+++ src/test/Coinchanger.t.sol
@@ -7,22 +7,45 @@ contract TestCoinchanger is CoinchangerTest {
     function test0PenniesFor0Coins() public {
         uint256 coins = 0;
 
-        _assertEqChange(coinchanger.makeChange(coins), Change(0));
+        Change memory change = coinchanger.makeChange(coins);
+
+        _assertEqChange(change, Change(0, 0));
     }
 
     function test1PennyFor1Coins() public {
         uint256 coins = 1;
 
-        _assertEqChange(coinchanger.makeChange(coins), Change(1));
+        Change memory change = coinchanger.makeChange(coins);
+
+        _assertEqChange(change, Change(1, 0));
     }
 
-    function test2PenniesFo2Coins() public {
+    function test2PenniesFor2Coins() public {
         uint256 coins = 2;
 
-        _assertEqChange(coinchanger.makeChange(coins), Change(2));
+        Change memory change = coinchanger.makeChange(coins);
+
+        _assertEqChange(change, Change(2, 0));
+    }
+
+    function test1NickleFor5Coins() public {
+        uint256 coins = 5;
+
+        Change memory change = coinchanger.makeChange(coins);
+
+        _assertEqChange(change, Change(0, 1));
+    }
+
+    function test1Nickle1PennyFor6Coins() public {
+        uint256 coins = 6;
+
+        Change memory change = coinchanger.makeChange(coins);
+
+        _assertEqChange(change, Change(1, 1));
     }
 
     function _assertEqChange(Change memory _changeA, Change memory _changeB) private {
         assertEq(_changeA.pennies, _changeB.pennies);
+        assertEq(_changeA.nickles, _changeB.nickles);
     }
 }
